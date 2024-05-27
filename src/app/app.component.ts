04ee2c2df3c08768da6a10f34329ded7b122220c
BrentSimons <r0878913@student.thomasmore.be>
2024-05-27 22:01:21 +0200

    Feature: Add internship section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,12 @@ import {SkillsComponent} from "./section/skills/skills.component";
 import {ProjectCardComponent} from "./component/project-card/project-card.component";
 import {NgForOf} from "@angular/common";
 import {ProjectsComponent} from "./section/projects/projects.component";
+import {InternshipComponent} from "./section/internship/internship.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroComponent, AboutMeComponent, EducationComponent, SkillsComponent, ProjectCardComponent, NgForOf, ProjectsComponent],
+  imports: [RouterOutlet, HeroComponent, AboutMeComponent, EducationComponent, SkillsComponent, ProjectCardComponent, NgForOf, ProjectsComponent, InternshipComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
