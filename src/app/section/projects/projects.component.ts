0802d186f863d7cd7e283fc75cacbd88aac70bdb
BrentSimons <r0878913@student.thomasmore.be>
2024-05-28 14:10:58 +0200

    Feature: Filter for projects

--- src/app/section/projects/projects.component.ts
+++ src/app/section/projects/projects.component.ts
@@ -1,23 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {projects} from "../../data/projects";
-import {NgForOf} from "@angular/common";
+import {NgClass, NgForOf, NgIf} from "@angular/common";
 import {ProjectCardComponent} from "../../component/project-card/project-card.component";
 import {Project} from "../../data/models/project-card.model";
+import {Category} from "../../data/models/category-model";
+
+type CategoryFilter = Category | "";
 
 @Component({
   selector: 'app-projects',
   standalone: true,
   imports: [
     NgForOf,
-    ProjectCardComponent
+    ProjectCardComponent,
+    NgClass,
+    NgIf
   ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
-export class ProjectsComponent {
-    protected readonly projects: Project[] = projects;
+export class ProjectsComponent implements OnInit {
+  public readonly projects: Project[] = projects;
+  public readonly categories: Category[] = [...new Set(projects.map(p => p.category))]; // Extract unique categories
 
-    protected moreThanFiveProjects(): boolean {
-        return this.projects.length >= 5;
+  protected filteredProjects: Project[] = [];
+  protected categoryFilter: CategoryFilter = "";
+
+  ngOnInit(): void {
+    this.filteredProjects = this.projects;
+  }
+
+  protected setCategoryFilter(category: CategoryFilter) {
+    this.categoryFilter = category;
+    this.filterProjects();
+  }
+
+  protected filterProjects(): void {
+    if (this.categoryFilter) {
+      this.filteredProjects = this.projects.filter(project => project.category === this.categoryFilter);
+    } else {
+      this.filteredProjects = this.projects;
     }
+  }
+
+  protected moreThanFiveProjects(): boolean {
+    return this.filteredProjects.length >= 5;
+  }
 }
