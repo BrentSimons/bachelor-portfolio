7a8f08ef6f4ae985ff7880784e09bdefbc242713
BrentSimons <r0878913@student.thomasmore.be>
2024-05-28 20:46:03 +0200

    Feat: Add meaningfull projects.

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -1,33 +1,19 @@
 import {Project} from "./models/project-card.model";
 
 export const projects: Project[] = [{
-  title: "Angular Project",
-  subtitle: "Angular project",
+  title: "Project PHP",
+  subtitle: "Groepsproject: zwemclub applicatie maken in Laravel",
   category: 'Front-end',
-  when: "year1",
-  description: "The description for the angular project",
-  mainImageLocation: "https://c8.alamy.com/comp/2AP458W/project-pipeline-management-poster-template-layout-business-idea-development-banner-booklet-leaflet-print-design-with-linear-icons-vector-brochur-2AP458W.jpg"
+  when: "year2",
+  description: "Tijdens het 2e jaar hebben we een groepsproject gemaakt in Laravel. We hebben een zwemclub applicatie gemaakt, ik heb mij binnen deze applicatie vooral gefocust op het maken van de webshop, maar ook heb ik designs gemaakt en geholpen met het ERD en de back-end.",
+  mainImageLocation: "./projects/php-zwemclub/PHP_homepage.png"
 }, {
-  title: "Springboot Project",
-  subtitle: "Springboot project",
+  title: "C# Training",
+  subtitle: "Groepsproject: OOP in C# training aan Wico Campus Neerpeelt",
   category: 'Back-end',
   when: "year2",
-  description: "The description for the springboot project",
-  mainImageLocation: "https://c8.alamy.com/comp/2AP458W/project-pipeline-management-poster-template-layout-business-idea-development-banner-booklet-leaflet-print-design-with-linear-icons-vector-brochur-2AP458W.jpg"
-}, {
-  title: "Springboot Project",
-  subtitle: "Springboot project",
-  category: 'Back-end',
-  when: "year2",
-  description: "The description for the springboot project",
-  mainImageLocation: "https://c8.alamy.com/comp/2AP458W/project-pipeline-management-poster-template-layout-business-idea-development-banner-booklet-leaflet-print-design-with-linear-icons-vector-brochur-2AP458W.jpg"
-}, {
-  title: "Springboot Project",
-  subtitle: "Springboot project",
-  category: 'Back-end',
-  when: "year2",
-  description: "The description for the springboot project",
-  mainImageLocation: "https://c8.alamy.com/comp/2AP458W/project-pipeline-management-poster-template-layout-business-idea-development-banner-booklet-leaflet-print-design-with-linear-icons-vector-brochur-2AP458W.jpg"
+  description: "Tijdens het 2e jaar moesten we voor een opdrachtgever een training geven, ik heb hiervoor mijn oude middelbare school gecontacteerd en een training-sessie georganiseerd over OOP in C#. Ook hebben we hiervoor zelf een cursus, oefeningen en presentatie voor gemaakt.",
+  mainImageLocation: "./projects/training/training.png"
 }]
 
 
