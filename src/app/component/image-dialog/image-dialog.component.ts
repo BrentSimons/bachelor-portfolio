4132ccaaae7aa52cfa4b80bc1db183dfc3fb98a9
BrentSimons <r0878913@student.thomasmore.be>
2024-06-03 18:42:31 +0200

    Improve project-details

---
+++ src/app/component/image-dialog/image-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './image-dialog.component.html',
+  styleUrl: './image-dialog.component.scss'
+})
+export class ImageDialogComponent {
+
+}
