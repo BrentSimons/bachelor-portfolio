773d6b4648f64062c618caac8ae4eedea276cc96
BrentSimons <r0878913@student.thomasmore.be>
2024-06-05 13:20:45 +0200

    Improve projects inhouds

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -4,18 +4,18 @@ export const projects: Project[] = [
   {
     id: 'zwemclub',
     title: 'Project PHP',
-    subtitle: 'Groepsproject: zwemclub applicatie maken in Laravel',
+    subtitle: 'Groepsproject: Zwemclub Applicatie Maken in Laravel',
     category: 'Front-end',
     when: 'year2',
     description:
-      'Tijdens het 2e jaar hebben we een groepsproject gemaakt in Laravel. We hebben een zwemclub applicatie gemaakt, ik heb mij binnen deze applicatie vooral gefocust op het maken van de webshop, maar ook heb ik designs gemaakt en geholpen met het ERD en de back-end.',
+      'In het tweede jaar hebben we een groepsproject uitgevoerd met Laravel. We hebben een zwemclub applicatie gemaakt, waarbij mijn focus lag op de webshop. Daarnaast heb ik designs gemaakt en geholpen met het ERD en de back-end.',
     mainImageLocation: './projects/php-zwemclub/PHP_homepage.jpg',
     technologies: ['Laravel', 'Tailwind'],
     teamProject: {
       isTeamProject: true,
       team: [
         {
-          name: 'Eva Burghoorn (Team leader)',
+          name: 'Eva Burghoorn (Teamleider)',
           linkedIn: 'https://www.linkedin.com/in/eva-burghoorn-432985230/',
         },
         {
@@ -38,8 +38,8 @@ export const projects: Project[] = [
       text: {
         title: 'Team Project PHP',
         description: [
-          'Voor dit project hadden we een team van 6 studenten. In het eerste semester hadden we een periode van een week om prototypes van de applicatie te maken, en een ERD uittewerken, in het tweede semester zouden elke week een volle dag op de campus ingepland krijgen om aan dit project werken.',
-          'De samenwerking voor dit team verliep goed, ik heb mij vooral bezig gehouden met het Webshop gedeelte van de applicatie. Dagelijks hadden we een standup meeting in de ochtend, met ook 2 meetings met de klant.',
+          'Ons team bestond uit zes studenten. In het eerste semester werkten we een week lang aan prototypes en een ERD. In het tweede semester werkten we wekelijks een volle dag op de campus aan dit project.',
+          'De samenwerking binnen het team verliep goed. Mijn hoofdverantwoordelijkheid was de webshop. Dagelijks hadden we een stand-up meeting in de ochtend en twee vergaderingen met de klant.',
         ],
         imageLocation: null,
       },
@@ -48,7 +48,7 @@ export const projects: Project[] = [
       {
         title: 'Webshop',
         description: [
-          'Voor dit project heb ik vooral aan de webshop gewerkt, dit was de uiteindelijke realisatie.',
+          'Mijn belangrijkste bijdrage aan dit project was de ontwikkeling van de webshop.',
         ],
         imageLocation: './projects/php-zwemclub/Webshop.jpg',
       },
@@ -58,11 +58,11 @@ export const projects: Project[] = [
   {
     id: 'training',
     title: 'C# Training',
-    subtitle: 'Groepsproject: OOP in C# training aan Wico Campus Neerpeelt',
+    subtitle: 'Groepsproject: OOP in C# Training bij Wico Campus Neerpelt',
     category: 'Back-end',
     when: 'year2',
     description:
-      'Tijdens het 2e jaar moesten we voor een opdrachtgever een training geven, ik heb hiervoor mijn oude middelbare school gecontacteerd en een training-sessie georganiseerd over OOP in C#. Ook hebben we hiervoor zelf een cursus, oefeningen en presentatie voor gemaakt.',
+      'In het tweede jaar moesten we een training geven voor een opdrachtgever. Ik heb hiervoor mijn oude middelbare school benaderd en een trainingssessie georganiseerd over OOP in C#. We hebben een cursus, oefeningen en een presentatie voorbereid.',
     mainImageLocation: './projects/training/training.jpg',
     technologies: ['Vitepress', 'Soft-skills', 'Organisational-skills'],
     teamProject: {
@@ -80,32 +80,32 @@ export const projects: Project[] = [
       text: {
         title: 'Team C# Training',
         description: [
-          'Voor deze training hadden we een team van 3 man, mezelf, Yarin en Wouter. Beide ik en Yarin waren oud-studenten van de middelbare school waar we de training zouden geven.',
+          'Ons team bestond uit drie personen: mezelf, Yarin en Wouter. Yarin en ik waren oud-studenten van de middelbare school waar we de training gaven.',
         ],
         imageLocation: null,
       },
     },
     textSections: [
       {
-        title: 'Organiseren',
+        title: 'Organisatie',
         description: [
-          'Voor het organiseren van deze training sessie heb ik contact opgenomen met mijn oude middelbare school Wico Campus Neerpelt. Zodra ik contact had met mijn oude leerkracht hebben we onderwerpen besproken waarover onze training zou kunnen gaan en uiteindelijk gekozen voor OOP in C#',
-          'Hierna heb ik ook het moment van de training geregeld op een moment dat zou passen voor hun en mijn andere teammates. Zodra de training ingepland was heb ik het initiatief genomen om aan de cursus te beginnen en de andere 2 studenten gevraagd om 2 oefeningen voortebereiden die we de volgers van de training gaan laten maken.',
-          'Ook heb ik het vervoer en de benodigde middelen geregeld: o.a. een beamer,  usb sticks, benodigde papieren, een gehostte cursus en met mijn teammates een presentatie voorbereid.',
+          'Voor het organiseren van deze trainingssessie nam ik contact op met mijn oude middelbare school Wico Campus Neerpelt. Na overleg met mijn oude leraar hebben we gekozen voor het onderwerp OOP in C#.',
+          'Ik plande de training op een geschikt moment voor zowel de school als mijn teamgenoten. Ik nam het initiatief om de cursus te ontwikkelen en vroeg mijn teamleden om oefeningen voor te bereiden voor de deelnemers.',
+          'Ik regelde ook de logistiek, zoals het vervoer en de benodigde apparatuur (zoals een beamer en USB-sticks), en we maakten gezamenlijk een presentatie.',
         ],
         imageLocation: './projects/training/campus-neerpelt.jpg',
       },
       {
-        title: 'Maken van de cursus',
+        title: 'Cursusontwikkeling',
         description: [
-          'Voor een online cursus te maken had ik gekozen om zelf een website hiervoor te maken met een technologie waarachter ik al even nieuwschierig was: Vitepress. Het leren en maken van deze cursus verliep vlotjes, de training werd gegeven aan de leerlingen van 6I&N, maar ik heb later ook nieuws gekregen dat de leerkracht mijn cursus ook heeft gebruikt om OOP aantebrengen aan de leerlingen van 5I&N',
+          'Voor de online cursus maakte ik een website met Vitepress, een technologie die ik wilde leren. De cursus werd gegeven aan de leerlingen van 6I&N, en later hoorde ik dat de leraar mijn cursus ook gebruikte om OOP aan 5I&N te onderwijzen.',
         ],
         imageLocation: './projects/training/cursus.jpg',
       },
       {
         title: 'Training',
         description: [
-          'De training zelf is ook vlotjes gelopen, we hebben doorheen het 5e en 6e lesuur de leerlingen van 6I&N aangeleerd hoe ze kunnen beginnen met OOP in C#. Eerst door de presentatie voortebrengen maar daarna door ze de oefeningen in de cursus te laten maken. Tijdens deze applicatie hebben we hun ook gebruik laten maken van Windows Forms als een extra uitdaging.',
+          'De training verliep soepel. We leerden de leerlingen van 6I&N hoe ze konden beginnen met OOP in C#. Na de presentatie maakten de leerlingen de oefeningen uit de cursus. We introduceerden ook Windows Forms als een extra uitdaging.',
         ],
         imageLocation: './projects/training/training.jpg',
       },
@@ -115,46 +115,43 @@ export const projects: Project[] = [
   {
     id: '20eeuw',
     title: 'Website 20e Eeuw',
-    subtitle:
-      'Soloproject: Website over de 20e eeuw, gemaakt in Bootstrap, HTML, JS',
+    subtitle: 'Soloproject: Website over de 20e Eeuw, gemaakt in Bootstrap, HTML, JS',
     category: 'Front-end',
     when: 'year1',
     description:
-      'Tijdens het 1e jaar hebben we een soloproject gemaakt in Bootstrap, HTML en JS. Ik heb een website gemaakt over de 20e eeuw, waarbij ik mij vooral heb gefocust op het design van de website.',
+      'In het eerste jaar maakte ik een soloproject met Bootstrap, HTML en JS. Ik creëerde een website over de 20e eeuw, waarbij mijn focus lag op het ontwerp.',
     mainImageLocation: './projects/20e-eeuw/home-page.jpg',
     technologies: ['Bootstrap', 'HTML', 'JavaScript'],
     teamProject: {
       isTeamProject: true,
       team: null,
       text: {
-        title: 'Team eeuw 20',
+        title: 'Team 20e Eeuw',
         description: [
-          'Voor dit project had ik een team van 3 man, echter voordat het project gestart werd had een van mijn teammates gestopt met de richting.',
-          'Mijn andere teammate kwam niet naar de les, en heeft ook nooit geholpen met het project.',
+          'Aanvankelijk had ik een team van drie, maar een van mijn teamleden stopte voordat het project begon. Mijn andere teamgenoot kwam niet naar de les en droeg niet bij aan het project.',
         ],
         imageLocation: null,
       },
     },
     textSections: [
       {
-        title: 'Home Page',
+        title: 'Homepagina',
         description: [
-          'Voor dit project heb ik de home page gemaakt. Hierin staat een korte uitleg over de 20e eeuw, dan een voorstelling van mij en mijn teammate, en een uitleg van elke subpagina.' +
-            'Ook kan je hier de Navbar en Footer die ik voor dit project hebt gemaakt zien',
+          'Ik maakte de homepagina met een korte uitleg over de 20e eeuw, een voorstelling van mij en mijn teamgenoot, en een beschrijving van elke subpagina. Hier zijn ook de navigatiebalk en de voettekst die ik heb gemaakt te zien.',
         ],
         imageLocation: './projects/20e-eeuw/home-page.jpg',
       },
       {
-        title: 'Koude oorlog',
+        title: 'Koude Oorlog',
         description: [
-          'Voor de subpagina van de koude oorlog heb ik ge-experimenteerd met een accordion object van bootstrap, hier heb ik ook custom styling aan gegeven.',
+          'Voor de subpagina over de Koude Oorlog experimenteerde ik met een accordion object van Bootstrap, waaraan ik ook custom styling toevoegde.',
         ],
         imageLocation: './projects/20e-eeuw/koude-oorlog.jpg',
       },
       {
-        title: 'Val Communisme',
+        title: 'Val van het Communisme',
         description: [
-          'Voor de val van communisme subpagina heb ik een custom timeline gemaakt. Hiervoor heb ik een Javascript functie geschreven die berekend hoelang geleden dit evenement is gebeurd en een het card object van bootstrap custom styling gegeven.',
+          'Voor de subpagina over de val van het communisme maakte ik een custom tijdlijn. Ik schreef een JavaScript functie die berekende hoe lang geleden dit evenement plaatsvond en gaf het bootstrap card object custom styling.',
         ],
         imageLocation: './projects/20e-eeuw/val-communisme.jpg',
       },
