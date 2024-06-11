ea1597cb71e0e37e43a49ef8ed395795f906261e
BrentSimons <r0878913@student.thomasmore.be>
2024-06-11 21:43:28 +0200

    Add skills

--- src/app/data/skills.ts
+++ src/app/data/skills.ts
@@ -1,44 +1,27 @@
 import { Skill } from './models/skill-model';
 
-export const Skills: Skill[] = [
-  {
-    percentage: 90,
-    name: 'Java | Springboot',
-  },
-  {
-    percentage: 70,
-    name: 'Angular',
-  },
-  {
-    percentage: 50,
-    name: 'React',
-  },
-  {
-    percentage: 50,
-    name: 'ASP.NET',
-  },
-  {
-    percentage: 50,
-    name: 'Laravel',
-  },
-  {
-    percentage: 65,
-    name: 'Working with LLMs',
-  },
-  {
-    percentage: 75,
-    name: 'Testing',
-  },
-  {
-    percentage: 80,
-    name: 'Working with Rest APIs',
-  },
-  {
-    percentage: 75,
-    name: 'Python',
-  },
-  {
-    percentage: 65,
-    name: 'Figma',
-  },
-];
+export const Skills: Skill[] = [{
+  percentage: 90, name: 'Java | Springboot'
+},{
+  percentage: 90, name: 'Learning new technologies'
+}, {
+  percentage: 80, name: 'Angular'
+}, {
+  percentage: 65, name: 'React'
+}, {
+  percentage: 50, name: 'ASP.NET'
+}, {
+  percentage: 40, name: 'Laravel'
+}, {
+  percentage: 70, name: 'Working with LLMs'
+}, {
+  percentage: 75, name: 'Testing'
+}, {
+  percentage: 85, name: 'Working with Rest APIs'
+}, {
+  percentage: 75, name: 'Python'
+}, {
+  percentage: 65, name: 'Figma'
+}, {
+  percentage: 70, name: 'Working with SQL, NoSQL'
+}];
